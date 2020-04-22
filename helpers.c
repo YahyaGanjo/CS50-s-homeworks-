b1b875d88fcacffdec992940a07e736e198faada
YahyaGanjo <YahyaGanjo@users.noreply.github.com>
2020-04-22 23:54:18 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -45,10 +45,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
 void sepiaformula (int x, int y, int z)
 {
-    int sepiaRed = round((0.393 * z) + (0.769 * y) + (0.189 * x));
-    int sepiaGreen = round((0.349 * z) + (0.686 * y) + (0.168 * x));
-    int sepiaBlue = round((0.272 * z) + (0.534 * y) + (0.131 * x));
-    z = sepiaRed;
-    y = sepiaGreen;
-    x = sepiaBlue;
+    z = round((0.393 * z) + (0.769 * y) + (0.189 * x));
+    y = round((0.349 * z) + (0.686 * y) + (0.168 * x));
+    x = round((0.272 * z) + (0.534 * y) + (0.131 * x));
 }
