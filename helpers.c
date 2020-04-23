9f1076b3c02d1b6af32296d5b6704fd612fb964c
YahyaGanjo <YahyaGanjo@users.noreply.github.com>
2020-04-23 00:19:29 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -25,7 +25,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-          sepiaformula(image[i][j].rgbtBlue, image[i][j].rgbtGreen, image[i][j].rgbtRed);  
+          sepiaformula(image[i][j].rgbtBlue, image[i][j].rgbtGreen, image[i][j].rgbtRed);
         }
     }
     return;
@@ -45,7 +45,22 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
 void sepiaformula (int x, int y, int z)
 {
-    z = round((0.393 * z) + (0.769 * y) + (0.189 * x));
-    y = round((0.349 * z) + (0.686 * y) + (0.168 * x));
-    x = round((0.272 * z) + (0.534 * y) + (0.131 * x));
+    int red = round((0.393 * z) + (0.769 * y) + (0.189 * x));
+    int green = round((0.349 * z) + (0.686 * y) + (0.168 * x));
+    int blue = round((0.272 * z) + (0.534 * y) + (0.131 * x));
+    if (red > 255)
+    {
+        z = 255;
+    }
+    if (green > 255)
+    {
+        y = 255;
+    }
+    if (blue > 255)
+    {
+        x = 255;
+    }
+    z = red;
+    y = green;
+    x = blue;
 }
