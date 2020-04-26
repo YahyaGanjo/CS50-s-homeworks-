05e63c49a167e75f2bd81081314a80e87aa8cd5e
YahyaGanjo <YahyaGanjo@users.noreply.github.com>
2020-04-26 01:31:32 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -72,7 +72,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE newimage[height - 1][width - 1];
+    RGBTRIPLE newimage[height][width];
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
